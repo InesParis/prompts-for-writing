263da4d09fc3ba344f1f8c9be05a5b0d2a094cdf
InesParis <inesparisarranz@gmail.com>
2024-07-30 15:32:22 +0200

    update js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function generatePrompt(event) {
   let topicsYouLoveInput = document.querySelector("#topics-you-love").value;
 
   let apiKey = "4f2360cc5d2fbf9f02a9o90ddad3f50t";
-  let prompt = `generate a writing prompt with the ${topicInput} using the information to set the scene, include ${fictionNonfictionInput} to decide with the prompt should make the writer write a fictional story or a nonfictional one and finally use ${topicsYouLoveInput} to adapt the writing prompt to what the writer really likes. include a first sentence to their writing to help them start.`;
+  let prompt = `generate a writing prompt with the ${topicInput} using the information to set the scene, include ${fictionNonfictionInput} to decide with the prompt should make the writer write a fictional story or a nonfictional one and finally use ${topicsYouLoveInput} to adapt the writing prompt to what the writer really likes.the prompt shouldn't be longer that three sentences. include a first sentence to their writing to help them start.`;
   let context =
     "You are a great English teacher that wants their students to achieve their maximum capacities and you want to help them in particular with their writing skills, generating prompts that are adapted to their needs and fun, the prompts should be short and give and encouraging first line in a new paragraph for the students to continue";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
